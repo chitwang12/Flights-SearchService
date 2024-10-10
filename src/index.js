47444a6c7f2fe0ff28f668032b3088c4b841653f
Chitwan Gandhi <chitwangandhi@Chitwans-MacBook-Air.local>
2024-10-10 16:07:58 +0530

    City Repo Database Calls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const colors = require("colors");
-const bodyParser = require("body-parser");
-const { PORT } = require("./config/serverConfig");
+const bodyParser = require('body-parser');
+const { PORT } = require('./config/serverConfig');
+const CityRepository = require('./repository/city-repository');
 
 const setupAndStartServer = async () => {
   //create the express Object
@@ -10,8 +11,10 @@ const setupAndStartServer = async () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
   
+
   app.listen(PORT, () => {
     console.log(`App is listening at ${PORT}`.yellow);
+
   });
 };
 
